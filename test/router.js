d2e27797f9f9856b64859b471619adda7055cc93
JPeer264 <jan.oster94@gmail.com>
2017-03-14 16:39:47 +0100

    Add: inquirer prompt

--- test/router.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava';
-import router from '../lib/router';
-
-test((t) => {
-  t.true(router);
-});
---
+++ test/sgcPrompt.js
@@ -0,0 +1,7 @@
+import test from 'ava';
+import prompt from '../lib/sgcPrompt';
+
+test((t) => {
+  prompt();
+  // t.true(prompt());
+});
