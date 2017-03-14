9814eef16dcd27083faf4abdbcd85748d0fde57a
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-03-14 23:39:16 +0100

    :white_check_mark: Test: add test for .sgcrc_default, updated test for package.json .sgc

--- test/getConfig.js
+++ test/getConfig.js
@@ -1,15 +1,43 @@
 import test from 'ava';
 import path from 'path';
 import json from 'json-extra';
+import fs from 'fs';
 import getConfig from '../lib/getConfig';
 
 const cwd = process.cwd();
 const fixtures = path.join(cwd, 'test', 'fixtures');
 
 test('read config from a specific path', (t) => {
-  t.deepEqual(getConfig(path.join(fixtures, '.sgcrc')), json.readToObjSync(path.join(fixtures, '.sgcrc')))
+  t.deepEqual(getConfig(path.join(fixtures, '.sgcrc')), json.readToObjSync(path.join(fixtures, '.sgcrc')));
+});
+
+test('read config from a .sgcrc_default', (t) => {
+  t.deepEqual(getConfig(), json.readToObjSync(path.join(cwd, '.sgcrc_default')));
 });
 
 test('read config from package.json', (t) => {
-  t.deepEqual(getConfig(), json.readToObjSync(path.join(fixtures, '.sgcrc')))
+  let sgcrc = json.readToObjSync(path.join(fixtures, '.sgcrc'));
+  let packageJson = json.readToObjSync(path.join(cwd, 'package.json'));
+  packageJson.sgc = sgcrc;
+
+  // copy package.json to package.json.back
+  Promise.resolve(fs.createReadStream(cwd + '/package.json')
+    .pipe(fs.createWriteStream(cwd + '/package.json.back')))
+    .then(() => {
+      // delete package.json
+      fs.unlink(cwd + '/package.json');
+    })
+    .then(() => {
+      // write new package.json with the sgc property
+      fs.writeFile(cwd + '/package.json', JSON.stringify(packageJson), () => {
+        // perform test
+        t.deepEqual(getConfig(), sgcrc);
+      });
+    }).then(() => {
+      // delete package.json
+      fs.unlink(cwd + '/package.json');
+    }).then(() => {
+      // restore package.json from package.json.back
+      fs.rename(cwd + '/package.json.back', cwd + '/package.json');
+    });
 });
