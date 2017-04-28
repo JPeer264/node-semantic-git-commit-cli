1800c8e6ac68e75d19af077347fe3b245a3a9fc4
JPeer264 <jan.oster94@gmail.com>
2017-04-28 18:41:45 +0200

    Fix: remove whitespace in <Type>(<scope>):

--- test/helper/combineTypeScope.js
+++ test/helper/combineTypeScope.js
@@ -3,7 +3,7 @@ import test from 'ava';
 import combineTypeScope from '../../lib/helpers/combineTypeScope';
 
 test('check if the combination is correct', (t) => {
-  t.is(combineTypeScope('Type:', '(scope)'), 'Type (scope):');
-  t.is(combineTypeScope('Type', '(scope)'), 'Type (scope)');
+  t.is(combineTypeScope('Type:', '(scope)'), 'Type(scope):');
+  t.is(combineTypeScope('Type', '(scope)'), 'Type(scope)');
   t.is(combineTypeScope('Type'), 'Type');
 });
