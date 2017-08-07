129ea62c277dbf4da7525a31020db2623fde5db9
aichbauer <rudolfson.junior@gmail.com>
2017-08-07 19:38:42 +0200

    Test: lowercase and uppercase

--- test/questions.js
+++ test/questions.js
@@ -66,6 +66,24 @@ test('check the values of the question object', (t) => {
   t.is(typeof questionsList, 'object');
 });
 
+test.only('TYPES | upperCase (default)', (t) => {
+  const sgc = getConfig(path.join(fixtures, '.sgcrc'));
+
+  const choicesList = choices(sgc);
+
+  t.is(choicesList[0].value, 'Add:');
+});
+
+test.only('TYPES | lowerCase', (t) => {
+  const sgc = getConfig(path.join(fixtures, '.sgcrc'));
+
+  sgc.lowercaseTypes = true;
+
+  const choicesList = choices(sgc);
+
+  t.is(choicesList[0].value, 'add:');
+});
+
 test('SCOPE | check if scope is off by default', (t) => {
   const config = getConfig();
   const questionsList = questions(config);
