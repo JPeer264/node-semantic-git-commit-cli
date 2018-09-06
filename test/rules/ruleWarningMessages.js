08ea1ed7dd95617c8b85523fc2c574cb4edbf571
JPeer264 <jan.oster94@gmail.com>
2018-09-06 12:11:24 +0200

    Feat: throw on not valid route

--- test/rules/ruleWarningMessages.js
+++ test/rules/ruleWarningMessages.js
@@ -13,3 +13,15 @@ test('ruleWarningMessages', (t) => {
   const messages = ruleWaringMessages('input.', config);
   t.deepEqual(messages, 'The commit message has to be at least 10 character, but is only 6 character long.\nThe commit message can not end with a dot\n');
 });
+
+test('should throw an error', (t) => {
+  const config = {
+    rules: {
+      notExisting: 10,
+    },
+  };
+
+  const error = t.throws(() => ruleWaringMessages('input.', config));
+
+  t.true(error instanceof Error);
+});
