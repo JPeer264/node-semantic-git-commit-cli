7d3004cf237a5f57ef9fe4d3261a0feed87a74ff
JPeer264 <jan.oster94@gmail.com>
2019-02-24 19:05:48 +0100

    Test: add more tests

--- test/questions.js
+++ test/questions.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import fs from 'fs-extra';
 import os from 'os';
 import path from 'path';
+import chalk from 'chalk';
 
 import { withEmoji, withoutEmoji } from './fixtures/questions';
 import getConfig from '../lib/getConfig';
@@ -66,6 +67,26 @@ test('check the values of the question object', (t) => {
   t.is(typeof questionsList, 'object');
 });
 
+test('alternative description', (t) => {
+  const sgc = getConfig(path.join(fixtures, '.sgcrc'));
+
+  sgc.types[0].description = undefined;
+
+  const choicesList = choices(sgc);
+
+  t.is(choicesList[0].name, `${chalk.bold('Add:')} `);
+});
+
+test('correct description', (t) => {
+  const sgc = getConfig(path.join(fixtures, '.sgcrc'));
+
+  sgc.types[0].description = 'lala land';
+
+  const choicesList = choices(sgc);
+
+  t.is(choicesList[0].name, `${chalk.bold('Add:')} ${'lala land'}`);
+});
+
 test('TYPES | upperCase (default)', (t) => {
   const sgc = getConfig(path.join(fixtures, '.sgcrc'));
 
@@ -84,11 +105,52 @@ test('TYPES | lowerCase', (t) => {
   t.is(choicesList[0].value, 'add:');
 });
 
+test('TYPE | just show if type has not been added', (t) => {
+  const config = getConfig();
+  const questionsList = questions(config);
+
+  t.is(questionsList[0].when(), true);
+});
+
+test('TYPE | not show if type has been added', (t) => {
+  const config = getConfig();
+  const questionsList = questions(config, { t: 'feat' });
+
+  t.is(questionsList[0].when(), false);
+});
+
 test('SCOPE | check if scope is off by default', (t) => {
   const config = getConfig();
   const questionsList = questions(config);
 
   t.is(questionsList[1].when(), false);
+});
+
+test('SCOPE | check if scope is off when it has been added by argv', (t) => {
+  const config = getConfig();
+  const questionsList = questions(config, { s: 'some scope' });
+
+  t.is(questionsList[1].when(), false);
+});
+
+test('SCOPE | check if scope is off when it has been added in config and argv', (t) => {
+  const config = getConfig();
+
+  config.scope = true;
+
+  const questionsList = questions(config, { s: 'some scope' });
+
+  t.is(questionsList[1].when(), false);
+});
+
+test('SCOPE | check if scope is on when it has been added just in config', (t) => {
+  const config = getConfig();
+
+  config.scope = true;
+
+  const questionsList = questions(config);
+
+  t.is(questionsList[1].when(), true);
 });
 
 test('SCOPE | check if scope validates correctly', (t) => {
@@ -99,7 +161,7 @@ test('SCOPE | check if scope validates correctly', (t) => {
   t.is(questionsList[1].validate('correct'), true);
 });
 
-test('COMMIT | validate functions in questions', (t) => {
+test('MESSAGE | validate functions in questions', (t) => {
   const config = getConfig();
   const questionsList = questions(config);
 
@@ -108,12 +170,33 @@ test('COMMIT | validate functions in questions', (t) => {
   t.is(questionsList[2].validate('This message has over 72 characters. So this test will definitely fail. I can guarantee that I am telling the truth', { type: 'Fix' }), 'The commit message is not allowed to be longer as 72 character, but is 120 character long. Consider writing a body.\n');
 });
 
-test('COMMIT | when and default functions in questions', (t) => {
+test('MESSAGE | do not show if there is the message in argv', (t) => {
+  const config = getConfig();
+  const questionsList = questions(config, { m: 'something' });
+
+  t.is(questionsList[2].when(), false);
+});
+
+test('MESSAGE | show if no argv has been added', (t) => {
+  const config = getConfig();
+  const questionsList = questions(config);
+
+  t.is(questionsList[2].when(), true);
+});
+
+test('EDITOR | when and default functions in questions', (t) => {
   const config = getConfig();
   const questionsList = questions(config);
 
   t.is(questionsList[4].when({ body: true }), true);
   t.is(questionsList[4].when({ body: false }), false);
+});
+
+test('EDITOR | should return formatted message', (t) => {
+  const config = getConfig();
+  const questionsList = questions(config);
+
+  t.is(questionsList[4].default({ message: 'message', type: 'type' }), 'type: message\n\n\n');
 });
 
 test('CONFIRM EDITOR | check if it shows if it has to', (t) => {
